66f9a970a466c1205de677388c3e529a2b4c88f6
janskia <janski.adam@gmail.com>
2023-05-10 20:55:46 +0200

    Imported SpeedWarping script.

---
+++ Runtime/SpeedWarping.cs
@@ -0,0 +1,138 @@
+using System.Linq;
+using UnityEngine;
+
+public class SpeedWarping : MonoBehaviour
+{
+    [Header("Model parts")]
+    [SerializeField] private Transform leftFoot;
+    [SerializeField] private Transform rightFoot;
+    [SerializeField] private Transform leftHand;
+    [SerializeField] private Transform rightHand;
+    [SerializeField] private Transform leftShoulder;
+    [SerializeField] private Transform rightShoulder;
+    [SerializeField] private Transform legsOrigin;
+
+    [Header("Tweakable parameters")]
+    [Range(0f, 1f)]
+    [SerializeField] private float footWeight = 1f;
+    [Range(0f, 1f)]
+    [SerializeField] private float handWeight = 0.3f;
+    [Range(-1f, 1f)]
+    [SerializeField] private float hipJump = -0.15f;
+    [SerializeField] private bool scaleSpreadByScale = true;
+    [SerializeField] private Vector3 legsOriginOffset = new Vector3(-0.4f, 0.2f, 0f);
+
+    [Header("Value")]
+    [Range(0f, 5f)]
+    [SerializeField] public float Spread;
+
+    private Animator animator;
+
+    private Vector3 LegsOrigin
+    {
+        get
+        {
+            return legsOrigin.position + legsOriginOffset;
+        }
+    }
+
+    private float ScaledSpread
+    {
+        get
+        {
+            if (scaleSpreadByScale)
+            {
+                return Spread / transform.localScale.z;
+            }
+            else
+            {
+                return Spread;
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Reset()
+    {
+        Setup();
+    }
+
+    [ContextMenu("Setup")]
+    private void Setup()
+    {
+        animator = GetComponent<Animator>();
+
+        SetupBone("LeftFoot", ref leftFoot);
+        SetupBone("RightFoot", ref rightFoot);
+        SetupBone("LeftHand", ref leftHand);
+        SetupBone("RightHand", ref rightHand);
+        SetupBone("Hips", ref legsOrigin);
+        SetupBone("LeftShoulder", ref leftShoulder);
+        SetupBone("RightShoulder", ref rightShoulder);
+
+        void SetupBone(string humanName, ref Transform boneTransform)
+        {
+            var humanBone = animator.avatar.humanDescription.human.Single(x => x.humanName == humanName);
+            
+            Transform[] allChildren = transform.GetComponentsInChildren<Transform>();
+            foreach (Transform child in allChildren)
+            {
+                if (child.name == humanBone.boneName)
+                {
+                    boneTransform = child;
+                    return;
+                }
+            }
+        }
+    }
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+        DebugGizmos.Clear();
+        Vector3 hipJumpOffset = Vector3.up * Mathf.Max(0, ScaledSpread - 1) * hipJump * transform.InverseTransformPoint(leftFoot.position).z * transform.InverseTransformPoint(rightFoot.position).z * transform.localScale.z;
+
+        animator.SetIKPositionWeight(AvatarIKGoal.LeftFoot, footWeight);
+        ProcessLimb(AvatarIKGoal.LeftFoot, leftFoot.position + hipJumpOffset, LegsOrigin + hipJumpOffset);
+
+        animator.SetIKPositionWeight(AvatarIKGoal.RightFoot, footWeight);
+        ProcessLimb(AvatarIKGoal.RightFoot, rightFoot.position + hipJumpOffset, LegsOrigin + hipJumpOffset);
+
+        animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, handWeight);
+        ProcessLimb(AvatarIKGoal.LeftHand, leftHand.position + hipJumpOffset, leftShoulder.position + hipJumpOffset);
+
+        animator.SetIKPositionWeight(AvatarIKGoal.RightHand, handWeight);
+        ProcessLimb(AvatarIKGoal.RightHand, rightHand.position + hipJumpOffset, rightShoulder.position + hipJumpOffset);
+
+        animator.bodyPosition = animator.bodyPosition + hipJumpOffset;
+
+        DebugGizmos.DrawWireSphere(leftFoot.position, Color.red);
+        DebugGizmos.DrawWireSphere(LegsOrigin, new Color(1, 1, 1, 0.5f), (LegsOrigin - leftFoot.position).magnitude);
+        DebugGizmos.DrawWireSphere(rightFoot.position, Color.red);
+        DebugGizmos.DrawWireSphere(LegsOrigin, new Color(1, 1, 1, 0.5f), (LegsOrigin - rightFoot.position).magnitude);
+    }
+
+    private void ProcessLimb(AvatarIKGoal ikGoal, Vector3 limbPosition, Vector3 origin)
+    {
+        Vector3 localLimbPosition = transform.InverseTransformPoint(limbPosition);
+        Vector3 localOrigin = transform.InverseTransformPoint(origin);
+
+        Vector3 scaledPosition = new Vector3(localLimbPosition.x, localLimbPosition.y, localLimbPosition.z * ScaledSpread);
+
+        DebugGizmos.DrawSphere(transform.InverseTransformPoint(scaledPosition), Color.magenta);
+
+        Vector3 legVector = localLimbPosition - localOrigin;
+        float legLength = legVector.magnitude;
+        Vector3 newVector = scaledPosition - localOrigin;
+        float newLength = newVector.magnitude;
+        Vector3 finalPosition = Vector3.Lerp(localOrigin, scaledPosition, legLength / newLength);
+        finalPosition = transform.TransformPoint(finalPosition);
+
+        DebugGizmos.DrawSphere(finalPosition, Color.blue);
+
+        animator.SetIKPosition(ikGoal, finalPosition);
+    }
+}
